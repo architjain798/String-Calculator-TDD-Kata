6be8912e5a0235a2ae838ca5a1fadcde303678eb
Archit Jain <architjain798@gmail.com>
2024-07-02 00:00:21 +0530

    Add test case for different delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,13 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     } else {
-        const nums = numbers.split(/[,|\n]/);
+        let delimiter = /[,|\n]/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n", 2);
+            delimiter = new RegExp(parts[0][2], 'g');
+            numbers = parts[1];
+        }
+        const nums = numbers.split(delimiter);
         let sum = 0;
         nums.forEach(num => {
             sum += parseInt(num, 10);
